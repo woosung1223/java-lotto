9090c7e8142febddb8170b1a8a30864d734e5a09
woosung1223 <dntjd991223@naver.com>
2022-12-02 23:46:44 +0900

    feat(LottoController): 컨트롤러 정의

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,30 @@
+package lotto.controller;
+
+import lotto.domain.LottoSystem;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+    private final InputView inputView = new InputView();
+    private final LottoSystem lottoSystem = new LottoSystem();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        startPayRoutine();
+        startPrizeRoutine();
+    }
+
+    public void startPayRoutine() {
+        int money = inputView.readMoney();
+        lottoSystem.putMoney(money);
+        outputView.printBuyResult(lottoSystem.getPurchasedLotto());
+    }
+
+    public void startPrizeRoutine() {
+        List<Integer> winningLotto = inputView.readWinningLotto();
+        int bonusNumber = inputView.readBonusNumber();
+        outputView.printStatistics(lottoSystem.getStatistics(winningLotto, bonusNumber));
+    }
+}
