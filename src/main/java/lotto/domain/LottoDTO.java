c60bbd2e60f4b64e9b4017f36b87ef6e8157e82c
woosung1223 <dntjd991223@naver.com>
2022-12-02 22:30:09 +0900

    feat(LottoDTO): 구매한 로또 정보를 출력하기 위한 DTO 생성

---
+++ src/main/java/lotto/domain/LottoDTO.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LottoDTO {
+    private final int lottoAmount;
+    private final List<Lotto> lotto;
+
+    public LottoDTO(int lottoAmount, List<Lotto> lotto) {
+        this.lottoAmount = lottoAmount;
+        this.lotto = lotto;
+    }
+
+    public int getLottoAmount() {
+        return lottoAmount;
+    }
+
+    public List<Lotto> getLotto() {
+        return Collections.unmodifiableList(lotto);
+    }
+}
